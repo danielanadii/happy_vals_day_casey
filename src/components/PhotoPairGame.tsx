12993c121c7bfaa6df333ada766a157bce8cd651
danielanadii <45839415+danielanadii@users.noreply.github.com>
2026-02-10 18:33:51 +0700

    Update PhotoPairGame.tsx fuuuugh

--- src/components/PhotoPairGame.tsx
+++ src/components/PhotoPairGame.tsx
@@ -93,13 +93,11 @@ export default function PhotoPairGame({
       {/* Image preload */}
       <div className="hidden">
         {images.map((image, i) => (
-          <Image
+          <img
             key={i}
             src={image}
             alt={`Image ${i + 1}`}
-            fill
             className="object-cover"
-            priority
           />
         ))}
       </div>
@@ -138,10 +136,9 @@ export default function PhotoPairGame({
                 transition={{ duration: 0.5 }}
                 style={{ backfaceVisibility: "hidden" }}
               >
-                <Image
+                <img
                   src={images[index]}
                   alt={`Imagen ${index + 1}`}
-                  fill
                   className="rounded-sm lg:rounded-md object-cover"
                 />
               </motion.div>
