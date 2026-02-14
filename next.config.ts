0455e5be2828099b07ea73a8e60b13ade49e97a6
Daniel <it-dev06@lgi.co.id>
2026-02-14 12:30:59 +0700

    update image

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '',
+  basePath: '/happy_vals_day_casey',
   assetPrefix: '/happy_vals_day_casey/',
   images: {
     unoptimized: true,
